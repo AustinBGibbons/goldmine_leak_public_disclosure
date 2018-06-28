8da7f6e5ec8e503aaa8f703a2d12974f0c64d79d
Samir Naik <snaik@plaid.com>
2018-06-28 15:53:58 -0700

    Added prism

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Prism from 'prismjs';
+import { AppState } from '../common';
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  renderAppState(appState) {
+    switch(appState) {
+      case AppState.INIT:
+        return (
+          <div>
+            <p className="welcome-msg">
+              Welcome to the Plaid Boilerplate Tutorial!
+            </p>
+            <p>
+              Step 1: Create a .env file with your API keys:
+            </p>
+            <pre>
+              <code class="language-bash">
+{`PORT=8000
+PLAID_CLIENT_ID=<your client id>
+PLAID_SECRET=<your secret>
+PLAID_PUBLIC_KEY=<your public key>
+PLAID_ENV=sandbox
+`}
+              </code>
+            </pre>
+            <a className="dashboard-link"
+               href="https://dashboard.plaid.com/account/keys"
+               target="_blank">
+              Click here to get your API Keys
+            </a>
+          </div>
+        );
+      case AppState.ITEM_LINKED:
+        return (
+          <div>
+            <p>Item Linked! Calling app server to exchange public_token for access_token...</p>
+          </div>
+        );
+      case AppState.PUBLIC_TOKEN_EXCHANGED:
+        return (
+          <div>
+            <p>Server has access_token! App server is waiting for transactions from Plaid...</p>
+          </div>
+        );
+      case AppState.TRANSACTIONS_RECEIVED:
+        return (
+          <div>
+            <p>Received transactions! See below</p>
+          </div>
+        )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="sidebar">
+          <div className="logo">
+            <a href="#">
+              Boilerplate App 💸⏱
+            </a>
+          </div>
+          <div className="app-state">
+            {this.renderAppState(this.props.appState)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
